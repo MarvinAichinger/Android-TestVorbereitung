5e8ffea78700ada33550ada2033896ea65d0f67c
MarvinAichinger <marvin.aichinger@outlook.com>
2022-12-11 16:20:29 +0100

    menu

--- app/src/main/java/com/example/myapplication/SettingsFragment.kt
+++ app/src/main/java/com/example/myapplication/SettingsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.myapplication.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -24,6 +26,7 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         //view?.findNavController()?.navigate(R.id.action_settingsFragment_to_editFragment)
         binding = FragmentSettingsBinding.inflate(inflater)
+        setupMenu()
         return binding.root
     }
 
@@ -32,6 +35,25 @@ class SettingsFragment : Fragment() {
         binding.changeButton.setOnClickListener {
             findNavController().navigate(R.id.action_settingsFragment_to_editFragment)
         }
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.helpFragment -> {
+                        return NavigationUI.onNavDestinationSelected(menuItem,findNavController())
+                    }
+                }
+                return false
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     companion object {
